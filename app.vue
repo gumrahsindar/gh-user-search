e7d0f3c2f0033e20bd693cd8f3084dc905c16ee4
gumrahsindar <gsindar@gmail.com>
2024-10-19 20:02:37 +0300

    style: add css reset, variables and utility classes

--- app.vue
+++ app.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>Hello World</div>
+  <main id="main">
+    <h1 class="title-xl">The Octocat</h1>
+  </main>
 </template>
+
+<style>
+#__nuxt {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 640px) {
+    align-items: flex-start;
+  }
+}
+</style>
