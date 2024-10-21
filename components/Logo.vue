6c99e315f0e12c859bb4930b4dd7a24cf59f7e6f
gumrahsindar <gsindar@gmail.com>
2024-10-21 22:07:49 +0300

    Add Logo and ThemeSwitch components to header

---
+++ components/Logo.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="title-lg">devfinder</h1>
+</template>
---
+++ components/ThemeSwitch.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="theme-switch">
+    <p>light</p>
+    <img src="/images/icon-sun.svg" alt="sun icon" />
+  </div>
+</template>
+
+<style scoped>
+.theme-switch {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+}
+</style>
