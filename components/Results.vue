aee6edb4776acda388dbc277af60aa41f55ee729
gumrahsindar <gsindar@gmail.com>
2024-10-22 21:20:34 +0300

    Refactor Results component to add user profile information and improve responsiveness

--- components/Results.vue
+++ components/Results.vue
@@ -1,60 +1,125 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { User } from '~/lib/types'
+
+// props
+const props = defineProps<{
+  user: User | null
+  isLoading: boolean
+}>()
+
+// convert the date to a readable format
+const date = computed(() => {
+  if (!props.user) return ''
+  return new Date(props.user.created_at).toLocaleDateString('en-GB', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  })
+})
+
+// remove the protocol and www from the URL
+function trimUrl(url: string): string {
+  return url.replace(/^(https?:\/\/)?(www\.)?/, '')
+}
+
+const trimmedBlogText = computed(() => {
+  if (!props.user || !props.user.blog) return ''
+  return trimUrl(props.user.blog)
+})
+</script>
 
 <template>
   <section id="results">
     <div class="results-container">
       <article class="results-grid-layout">
-        <img class="avatar" src="/images/octocat.png" alt="profile" />
+        <img
+          class="avatar"
+          :src="user?.avatar_url"
+          width="117"
+          height="117"
+          alt="profile"
+        />
         <div class="user-credential">
           <div>
-            <h2>The Octocat</h2>
-            <small>@octocat</small>
+            <h2>{{ user?.name }}</h2>
+            <small
+              ><a :href="user?.html_url" target="_blank"
+                >@{{ user?.login }}</a
+              ></small
+            >
           </div>
           <time
+            v-if="user?.created_at"
             class="joined-date"
-            datetime="
-            2011-01-25T00:00:00Z
-          "
-            >Joined 25 Jan 2011
+            :datetime="new Date(user.created_at).toISOString().split('T')[0]"
+          >
+            Joined {{ date }}
           </time>
         </div>
-        <p class="user-bio text-base">
+        <p v-if="user?.bio" class="user-bio text-base">
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
           Quisque volutpat mattis eros.
         </p>
+        <NotAvailable v-else class="user-bio text-base"
+          >This profile has no bio</NotAvailable
+        >
         <div class="user-stats">
           <div>
             <h3 class="title-sm">Repos</h3>
-            <p class="text-lg">8</p>
+            <p class="text-lg">{{ user?.public_repos }}</p>
           </div>
           <div>
             <h3 class="title-sm">Followers</h3>
-            <p class="text-lg">3938</p>
+            <p class="text-lg">{{ user?.followers }}</p>
           </div>
           <div>
             <h3 class="title-sm">Following</h3>
-            <p class="text-lg">9</p>
+            <p class="text-lg">{{ user?.following }}</p>
           </div>
         </div>
         <div class="user-link">
           <div class="flex-item">
             <div>
-              <Icons iconSrc="location" />
-              <p>San Francisco</p>
+              <Icons
+                :class="{ 'not-available': !user?.location }"
+                iconSrc="location"
+              />
+              <p v-if="user?.location">
+                {{ user.location }}
+              </p>
+              <NotAvailable v-else />
             </div>
             <div>
-              <Icons iconSrc="website" />
-              <a href="https://github.blog">github.blog</a>
+              <Icons
+                :class="{ 'not-available': !user?.blog }"
+                iconSrc="website"
+              />
+              <a v-if="user?.blog" target="_blank" :href="user?.blog">{{
+                trimmedBlogText
+              }}</a>
+              <NotAvailable v-else />
             </div>
           </div>
           <div class="flex-item">
             <div>
-              <Icons iconSrc="twitter" />
-              <a href="#">Not Available</a>
+              <Icons
+                :class="{ 'not-available': !user?.twitter_username }"
+                iconSrc="twitter"
+              />
+              <a
+                v-if="user?.twitter_username"
+                :href="`https://x.com/${user?.twitter_username}`"
+                >@{{ user?.twitter_username }}</a
+              >
+              <NotAvailable v-else />
             </div>
             <div>
-              <Icons iconSrc="company" />
-              <p>@github</p>
+              <Icons
+                :class="{ 'not-available': !user?.company }"
+                iconSrc="company"
+              />
+              <p v-if="user?.company">{{ user?.company }}</p>
+              <NotAvailable v-else />
             </div>
           </div>
         </div>
@@ -88,15 +153,24 @@
     & .avatar {
       grid-column: 1 / 2;
       grid-row: 1 / 3;
+      border-radius: 50%;
+      height: auto;
 
       @media (max-width: 64rem) {
         grid-row: 1 / 2;
       }
+
+      @media (max-width: 40rem) {
+        min-width: 80px;
+        min-height: 80px;
+      }
     }
 
     & .user-credential {
+      align-self: flex-end;
       margin-bottom: 20px;
       grid-column: 2 / -1;
+      gap: 6px;
       grid-row: 1 / 2;
       display: flex;
       flex-wrap: wrap;
@@ -108,8 +182,34 @@
         align-items: flex-start;
       }
 
+      @media (max-width: 40rem) {
+        justify-content: flex-start;
+        margin-left: 1rem;
+      }
+
+      @media (max-width: 25rem) {
+        margin-left: 20px;
+      }
+
+      & h2 {
+        @media (max-width: 40rem) {
+          font-size: 1rem;
+        }
+      }
+
       & small {
         color: var(--clr-vivid-blue);
+        font-size: 1rem;
+
+        @media (max-width: 40rem) {
+          font-size: 0.8125rem;
+        }
+      }
+
+      & .joined-date {
+        @media (max-width: 40rem) {
+          font-size: 0.8125rem;
+        }
       }
     }
 
@@ -118,7 +218,13 @@
       margin-bottom: 32px;
 
       @media (max-width: 64rem) {
+        margin-top: 24px;
         grid-column: 1 / -1;
+      }
+
+      @media (max-width: 40rem) {
+        margin-bottom: 24px;
+        font-size: 13px;
       }
     }
 
@@ -134,15 +240,29 @@
       gap: 24px;
 
       @media (max-width: 64rem) {
+        margin-bottom: 30px;
         grid-column: 1 / -1;
+      }
+
+      @media (max-width: 40rem) {
+        margin-bottom: 24px;
+        padding: 16px;
       }
 
       & .title-sm {
         font-weight: normal;
+
+        @media (max-width: 40rem) {
+          font-size: 0.8125rem;
+        }
       }
 
       & .text-lg {
         color: var(--heading-color);
+
+        @media (max-width: 40rem) {
+          font-size: 1rem;
+        }
       }
 
       & div {
@@ -154,7 +274,8 @@
     & .user-link {
       grid-column: 2 / -1;
       display: flex;
-      gap: 62px;
+      justify-content: space-around;
+      gap: 1rem;
 
       @media (max-width: 64rem) {
         grid-column: 1 / -1;
@@ -163,6 +284,7 @@
       @media (max-width: 40rem) {
         flex-direction: column;
         gap: 16px;
+        font-size: 13px;
       }
 
       & .flex-item {
