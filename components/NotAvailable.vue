430633d8fa2f728b5d9118d898596a0f3f2445d7
gumrahsindar <gsindar@gmail.com>
2024-10-22 21:20:23 +0300

    create seperate components for icons and not available message to prevent code repeat

---
+++ components/NotAvailable.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p class="not-available">
+    <slot>Not Available</slot>
+  </p>
+</template>
+
+<style scoped>
+.not-available {
+  opacity: 0.5;
+  text-wrap: nowrap;
+}
+</style>
