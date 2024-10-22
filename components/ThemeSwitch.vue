3ef757154feb717e145fae09fab82b9260ea76f3
gumrahsindar <gsindar@gmail.com>
2024-10-22 21:21:08 +0300

    add theme toggle functionality and improve accessibility

--- components/ThemeSwitch.vue
+++ components/ThemeSwitch.vue
@@ -1,16 +1,76 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// add switch theme logic
+const theme = ref('light')
+const toggleTheme = () => {
+  const html = document.documentElement
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark')
+    html.classList.add('light')
+    theme.value = 'light'
+  } else {
+    html.classList.remove('light')
+    html.classList.add('dark')
+    theme.value = 'dark'
+  }
+}
+
+// check if the user prefers dark mode
+const switchState = ref(false)
+
+onMounted(() => {
+  const prefersDarkScheme = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches
+  switchState.value = prefersDarkScheme
+})
+
+watch(switchState, (newState) => {
+  if (newState) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+})
+</script>
 
 <template>
   <div class="theme-switch">
-    <p>light</p>
-    <img src="/images/icon-sun.svg" alt="sun icon" />
+    <button @click="toggleTheme" class="btn" aria-label="Toggle theme">
+      <span>
+        {{ theme === 'light' ? 'dark' : 'light' }}
+      </span>
+      <img
+        :src="`/images/icon-${theme === 'light' ? 'moon' : 'sun'}.svg`"
+        alt="sun icon"
+        width="20"
+        height="20"
+      />
+    </button>
   </div>
 </template>
 
 <style scoped>
 .theme-switch {
-  display: flex;
-  align-items: center;
-  gap: 16px;
+  & .btn {
+    display: flex;
+    gap: 16px;
+    align-items: center;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+    transition: opacity 0.2s;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+
+  & span {
+    text-transform: uppercase;
+    font-size: 13px;
+    letter-spacing: 2.5px;
+    color: var(--text-color);
+    font-weight: bold;
+  }
 }
 </style>
